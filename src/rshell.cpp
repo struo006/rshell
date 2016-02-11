1188156749e3603e57c15e91205d29c3c887692a
Joshua Woo-Sam <jwoos001@hammer.cs.ucr.edu>
2016-02-11 11:00:57 -0800

    Completed parsing and comments

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -7,24 +7,41 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<cstring> 
+#include<boost/tokenizer.hpp>
+#include<algorithm>
+#include<iterator>
+#include<sstream>
 
 using namespace std;
+using namespace boost;
 
  int main()
 {	
 	string str;	
-	string arg = '';
+	string arg = "";
 	 while(1)
 	{		
 		cout << "$"; 
-		getline(cin, str); 
+		vector <string> line;
+		getline(cin,str);
 		if(str == "exit")		
 		{
 			 break;		
 		}
-		else		
+		for(int i = 0; i < str.size(); i++)
 		{
-			execvp(str.c_str(), arg.c_str());					}	
+			if(str.at(i) == '#')
+			{
+				str = str.substr(0,i);
+			}
+		}
+		istringstream iss(str);
+		copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(line));
+		
+		for(int i = 0; i < line.size(); i++)
+		{
+			cout << line.at(i) << endl;
+		}
 	}
 	  return 0;
 }
